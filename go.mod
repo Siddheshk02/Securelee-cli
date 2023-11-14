31a47aaaca3a2aea656b119386b8d14e10c9613b
Siddhesh Khandagale <khandagalesid02@gmail.com>
2023-11-14 19:06:49 +0530

    Final Update

--- go.mod
+++ go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/joho/godotenv v1.5.1
+	// github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mbndr/figlet4go v0.0.0-20190224160619-d6cef5b186ea
--- lib/init.go
+++ lib/init.go
@@ -1,10 +1,6 @@
 package lib
 
 import (
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
 	"github.com/pangeacyber/pangea-go/pangea-sdk/v2/pangea"
 	"github.com/pangeacyber/pangea-go/pangea-sdk/v2/service/authn"
 	"github.com/pangeacyber/pangea-go/pangea-sdk/v2/service/vault"
@@ -14,40 +10,44 @@ func Init() *authn.AuthN {
 	// ctx, cancelFn := context.WithTimeout(context.Background(), 60*time.Second)
 	// defer cancelFn()
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("\033[31m", "Error loading .env file", "\033[0m")
-	}
-	// Get token
-	token := os.Getenv("PANGEA_AUTHN_TOKEN")
-	if token == "" {
-		log.Fatal("\033[31m", "Unauthorized: No token present", "\033[0m")
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatal("\033[31m", "Error loading .env file", "\033[0m")
+	// }
+	// // Get token
+	// token := os.Getenv("PANGEA_AUTHN_TOKEN")
+	// if token == "" {
+	// 	log.Fatal("\033[31m", "Unauthorized: No token present", "\033[0m")
+	// }
+	token := "pts_xajlrac4we4mufoebqgejbrh2ieq72c4"
 
 	// Create config and client
+	domain := "aws.us.pangea.cloud"
 	client := authn.New(&pangea.Config{
 		Token:  token,
-		Domain: os.Getenv("PANGEA_DOMAIN"),
+		Domain: domain,
 	})
 
 	return client
 }
 
 func InitVault() vault.Client {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("\033[31m", "Error loading .env file", "\033[0m")
-	}
-	// Get token
-	token := os.Getenv("PANGEA_VAULT_TOKEN")
-	if token == "" {
-		log.Fatal("\033[31m", "Unauthorized: No token present", "\033[0m")
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatal("\033[31m", "Error loading .env file", "\033[0m")
+	// }
+	// // Get token
+	// token := os.Getenv("PANGEA_VAULT_TOKEN")
+	// if token == "" {
+	// 	log.Fatal("\033[31m", "Unauthorized: No token present", "\033[0m")
+	// }
+	token := "pts_xajlrac4we4mufoebqgejbrh2ieq72c4"
 
 	// Create config and client
+	domain := "aws.us.pangea.cloud"
 	client := vault.New(&pangea.Config{
 		Token:  token,
-		Domain: os.Getenv("PANGEA_DOMAIN"),
+		Domain: domain,
 	})
 
 	return client
